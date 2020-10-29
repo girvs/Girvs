1df1f0ca23b58e812b2404e128414845255c0975
kicck <eternal520@gmail.com>
2020-10-29 15:05:10 +0800

    修复动态API BUG

---
+++ Girvs.WebFrameWork/Plugins/DynamicWebApi/DynamicWebApiStartup.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Girvs.Domain.Configuration;
+using Girvs.Domain.Infrastructure;
+using Girvs.Domain.TypeFinder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Panda.DynamicWebApi;
+
+namespace Girvs.WebFrameWork.Plugins.DynamicWebApi
+{
+    public class DynamicWebApiStartup : IPluginStartup
+    {
+        public string Name { get; } = "DynamicWeb";
+        public bool Enabled
+        {
+            get
+            {
+                var config = EngineContext.Current.Resolve<GirvsConfig>();
+                var funcConfig = config.FunctionalModules.SingleOrDefault(x => x.Name == Name);
+                return funcConfig != null && funcConfig.Enabled;
+            }
+        }
+
+        public void ConfigureServicesRegister(IServiceCollection services, ITypeFinder typeFinder, GirvsConfig config)
+        {
+            services.AddDynamicWebApi();
+        }
+
+        public void ConfigureRequestPipeline(IApplicationBuilder application)
+        {
+        }
+
+        public void ConfigureMapEndpointRoute(IEndpointRouteBuilder builder)
+        {
+        }
+
+        public int Order { get; } = 112;
+    }
+}
