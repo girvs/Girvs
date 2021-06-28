4a7db79f236975f5aeed8a1af7f2aad7387530e0
DESKTOP-409UDMT\Administrator <86969788@qq.com>
2021-06-28 17:15:04 +0800

    添加Swagger

---
+++ Net5/Girvs.Swagger/SwaggerModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Girvs.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Girvs.Swagger
+{
+    public class SwaggerModule: IAppModuleStartup
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Configure(IApplicationBuilder application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfigureMapEndpointRoute(IEndpointRouteBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Order { get; }
+    }
+}
