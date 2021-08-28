a393072569e632a0066f056cf9b2ed4feb947dc7
kicck <eternal520@gmail.com>
2021-08-28 14:15:19 +0800

    修复BUG

--- Girvs.Driven/Events/Message.cs
+++ Girvs.Driven/Events/Message.cs
@@ -21,7 +21,7 @@ namespace Girvs.Driven.Events
                 MessageSource.SourceNameId = EngineContext.Current.ClaimManager.GetUserId();
                 MessageSource.TenantId = EngineContext.Current.ClaimManager.GetTenantId();
                 MessageSource.TenantName = EngineContext.Current.ClaimManager.GetTenantName();
-                MessageSource.IpAddress = EngineContext.Current.HttpContext?.Request.GetUserRemoteIpAddress();
+                MessageSource.IpAddress = EngineContext.Current.HttpContext?.Request.GetApiGateWayRemoteIpAddress();
             }
             finally
             {
