e93ee5ad1a60295f2d31bec4ff3d1c6e6ccdd43c
kicck <eternal520@gmail.com>
2021-12-03 16:46:10 +0800

    查看保存时的日志相关信息

--- Girvs.EntityFrameworkCore/UoW/UnitOfWork.cs
+++ Girvs.EntityFrameworkCore/UoW/UnitOfWork.cs
@@ -8,18 +8,21 @@ using Girvs.EntityFrameworkCore.Context;
 using Girvs.Infrastructure;
 using Girvs.TypeFinder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Girvs.EntityFrameworkCore.UoW
 {
     public class UnitOfWork<TEntity> : IUnitOfWork<TEntity> where TEntity : Entity
     {
         private readonly IDbContext _context;
+        private readonly ILogger<UnitOfWork<TEntity>> _logger;
 
         //构造函数注入
         public UnitOfWork()
         {
             _context = GetRelatedDbContext() ?? throw new ArgumentNullException(nameof(DbContext));
             _context.SwitchMasterDataBase();
+            _logger = EngineContext.Current.Resolve<ILogger<UnitOfWork<TEntity>>>();
         }
 
         IDbContext GetRelatedDbContext()
@@ -41,7 +44,11 @@ namespace Girvs.EntityFrameworkCore.UoW
 
         public async Task<bool> Commit(CancellationToken cancellationToken = new CancellationToken())
         {
-            return await _context.SaveChangesAsync(cancellationToken) > 0;
+            var changeRowCount = await _context.SaveChangesAsync(cancellationToken);
+            _logger.LogInformation(
+                $"提交保存时的DBContextId为：{(_context as DbContext)?.ContextId.InstanceId.ToString()},保存时影响的行数为：{changeRowCount}");
+
+            return changeRowCount > 0;
         }
     }
 }
