dc1a448bd9431f25bdf88a42b9b47c0800d13a0c
kicck <eternal520@gmail.com>
2021-12-08 14:03:02 +0800

    修改定时任务作用域问题

--- Girvs.Quartz/GirvsJob.cs
+++ Girvs.Quartz/GirvsJob.cs
@@ -1,19 +1,25 @@
 using System;
 using System.Threading.Tasks;
+using Girvs.Infrastructure;
 using Girvs.Infrastructure.GirvsServiceContext;
 using Quartz;
 
 namespace Girvs.Quartz
 {
-    public abstract class GirvsJob : IJob
+    public abstract class GirvsJob : IJob,IDisposable
     {
         public GirvsJob(
             IServiceProvider serviceProvider
         )
         {
-            ServiceContextFactory.Create(serviceProvider);
+            EngineContext.Current.SetCurrentThreadServiceProvider(serviceProvider);
         }
 
         public abstract Task Execute(IJobExecutionContext context);
+
+        public void Dispose()
+        {
+            EngineContext.Current.SetCurrentThreadServiceProvider(null);
+        }
     }
 }
