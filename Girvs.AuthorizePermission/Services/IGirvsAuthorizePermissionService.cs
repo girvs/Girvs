de746cdb8b7e9f5c94927593fa114939ad2261ac
kicck <eternal520@gmail.com>
2022-02-25 15:17:35 +0800

    IOC注册IGirvsAuthorizePermissionService接口

--- Girvs.AuthorizePermission/Services/IGirvsAuthorizePermissionService.cs
+++ Girvs.AuthorizePermission/Services/IGirvsAuthorizePermissionService.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Girvs.BusinessBasis;
 using Girvs.DynamicWebApi;
 
 namespace Girvs.AuthorizePermission.Services
 {
-    public interface IGirvsAuthorizePermissionService : IAppWebApiService
+    public interface IGirvsAuthorizePermissionService : IAppWebApiService, IManager
     {
         Task<List<AuthorizePermissionModel>> GetAuthorizePermissionList();
 
