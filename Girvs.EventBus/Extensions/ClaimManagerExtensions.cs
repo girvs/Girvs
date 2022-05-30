ad1b8d6c1884826fab9715c8ab8e87b0b206f377
kicck <eternal520@gmail.com>
2022-05-30 15:45:10 +0800

    事件重新设置请求头移除多余，不必要的

--- Girvs.EventBus/Extensions/ClaimManagerExtensions.cs
+++ Girvs.EventBus/Extensions/ClaimManagerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using DotNetCore.CAP;
+using DotNetCore.CAP.Messages;
 using Girvs.Extensions;
 using Girvs.Infrastructure;
 
@@ -7,9 +8,23 @@ namespace Girvs.EventBus.Extensions
 {
     public static class ClaimManagerExtensions
     {
+        /// <summary>
+        /// /重新设置请求头
+        /// </summary>
+        /// <param name="claimManager"></param>
+        /// <param name="capHeader"></param>
         public static void CapEventBusReSetClaim(this IGirvsClaimManager claimManager, CapHeader capHeader)
         {
             var claimdic = capHeader.ToDictionary(x => x.Key, v => v.Value);
+            claimdic.Remove(Headers.MessageId);
+            claimdic.Remove(Headers.MessageName);
+            claimdic.Remove(Headers.Group);
+            claimdic.Remove(Headers.Type);
+            claimdic.Remove(Headers.CorrelationId);
+            claimdic.Remove(Headers.CorrelationSequence);
+            claimdic.Remove(Headers.CallbackName);
+            claimdic.Remove(Headers.SentTime);
+            claimdic.Remove(Headers.Exception);
             claimdic.SetDictionaryKeyValue(GirvsIdentityClaimTypes.IdentityType,
                 IdentityType.EventMessageUser.ToString());
             claimManager.SetFromDictionary(claimdic);
