25b22caba9162e99233ad882123bef9478c37f6a
kicck <eternal520@gmail.com>
2022-09-07 09:49:31 +0800

    发布6.0 prview.1.0

--- Girvs.AuthorizePermission/ServicePermissionDescriptorAttribute.cs
+++ Girvs.AuthorizePermission/ServicePermissionDescriptorAttribute.cs
@@ -3,14 +3,34 @@
 [AttributeUsage(AttributeTargets.Class)]
 public class ServicePermissionDescriptorAttribute : System.Attribute
 {
-    public ServicePermissionDescriptorAttribute(string serviceName, string serviceId, string tag = "",
-        int order = 0, SystemModule systemModule = SystemModule.All, params string[] otherParams)
+    // public ServicePermissionDescriptorAttribute(string serviceName, string serviceId, string tag = "",
+    //     int order = 0, SystemModule systemModule = SystemModule.All, params string[] otherParams)
+    // {
+    //     ServiceId = Guid.Parse(serviceId);
+    //     ServiceName = serviceName;
+    //     Tag = tag;
+    //     Order = order;
+    //     SystemModule = systemModule;
+    //     OtherParams = otherParams;
+    // }
+
+    /// <summary>
+    /// 功能菜单权限
+    /// </summary>
+    /// <param name="serviceName">服务名称</param>
+    /// <param name="serviceId">服务Id</param>
+    /// <param name="tag">所属标签</param>
+    /// <param name="systemModule">所属模块</param>
+    /// <param name="order">排序</param>
+    /// <param name="otherParams">其它参数</param>
+    public ServicePermissionDescriptorAttribute(string serviceName, string serviceId, string tag,
+        SystemModule systemModule = SystemModule.All,int order = 0,  params string[] otherParams)
     {
         ServiceId = Guid.Parse(serviceId);
         ServiceName = serviceName;
         Tag = tag;
-        Order = order;
         SystemModule = systemModule;
+        Order = order;
         OtherParams = otherParams;
     }
 
@@ -20,8 +40,7 @@ public class ServicePermissionDescriptorAttribute : System.Attribute
     /// <summary>
     /// 所属标签
     /// </summary>
-    public string Tag { get; private set; }
-
+    public string Tag { get; private set; } 
     /// <summary>
     /// 排序
     /// </summary>
