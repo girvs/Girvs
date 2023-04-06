969fc49c5fc69c35388253b2f1c5f98b5670c139
kicck <eternal520@gmail.com>
2023-04-06 16:28:00 +0800

    修改认证支持IDS4

--- Girvs.AuthorizePermission/GirvsAuthorizeModule.cs
+++ Girvs.AuthorizePermission/GirvsAuthorizeModule.cs
@@ -33,32 +33,48 @@ public class GirvsAuthorizeModule : IAppModuleStartup
             AuthorizationModel.IdentityServer4)
         {
             authenticationBuilder
-                .AddJwtBearer(GirvsAuthenticationScheme.GirvsIdentityServer4, x =>
+                .AddJwtBearer(GirvsAuthenticationScheme.GirvsIdentityServer4, options =>
                 {
-                    //使用应用密钥得到一个加密密钥字节数组
-                    var key = Encoding.ASCII.GetBytes(authorizeConfig.IdentityServer4Config.ApiSecret +
-                                                      authorizeConfig.IdentityServer4Config.ApiSecret);
-                    x.RequireHttpsMetadata = true;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
+                    options.Authority = authorizeConfig.IdentityServer4Config.ServerHost;
+                    options.RequireHttpsMetadata = authorizeConfig.IdentityServer4Config.UseHttps;
+                    options.SaveToken = false;
+                    options.Audience = authorizeConfig.IdentityServer4Config.ApiResourceName;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = false,
                         ValidateAudience = false
                     };
-                    x.Events = new JwtBearerEvents();
                 });
-            // authenticationBuilder
-            //     .AddIdentityServerAuthentication(GirvsAuthenticationScheme.GirvsIdentityServer4, options =>
+
+            // services.AddAuthorization(options =>
+            // {
+            //     options.AddPolicy(config.Service.PolicyName, policy =>
             //     {
-            //         //由于名称过长，暂时替换
+            //         policy.RequireAuthenticatedUser();
+            //         policy.RequireClaim("scope", config.Service.ServiceName);
+            //     });
+            // });
+
+            // services.AddAuthentication("token")
+            //
+            //     // JWT tokens
+            //     .AddJwtBearer("token", options =>
+            //     {
             //         options.Authority = authorizeConfig.IdentityServer4Config.ServerHost;
+            //         options.Audience = authorizeConfig.IdentityServer4Config.ApiResourceName;
             //         options.RequireHttpsMetadata = authorizeConfig.IdentityServer4Config.UseHttps;
-            //         options.SupportedTokens = SupportedTokens.Both;
-            //         options.ApiSecret = authorizeConfig.IdentityServer4Config.ApiSecret;
-            //         options.ApiName = authorizeConfig.IdentityServer4Config.ApiResourceName;
-            //         options.IntrospectionDiscoveryPolicy.RequireKeySet = false;
+            //         //options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+            //
+            //         // if token does not contain a dot, it is a reference token
+            //         //options.ForwardDefaultSelector = Selector.ForwardReferenceToken("introspection");
+            //     })
+            //
+            //     // reference tokens
+            //     .AddOAuth2Introspection("introspection", options =>
+            //     {
+            //         options.Authority = authorizeConfig.IdentityServer4Config.ServerHost;
+            //         options.ClientId = authorizeConfig.IdentityServer4Config.ApiResourceName;
+            //         options.ClientSecret = authorizeConfig.IdentityServer4Config.ApiSecret;
             //     });
         }
     }
