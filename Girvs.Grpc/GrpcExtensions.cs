6f3f2108498d9554147e446c2a49ab4b3a791f5f
kicck <eternal520@gmail.com>
2024-06-18 16:22:56 +0800

    修改Grpc相关的信息

--- Girvs.Grpc/GrpcExtensions.cs
+++ Girvs.Grpc/GrpcExtensions.cs
@@ -10,17 +10,31 @@ public static class GrpcExtensions
         var logger = EngineContext.Current.Resolve<ILogger<object>>();
         foreach (var grpcService in grpcServices)
         {
-            logger.LogInformation($"注册GRPC服务：{grpcService.FullName}");
+            logger.LogInformation($"开始注册GRPC服务：{grpcService.FullName}");
             var method = typeof(GrpcEndpointRouteBuilderExtensions)
                 .GetMethod(nameof(GrpcEndpointRouteBuilderExtensions.MapGrpcService))
                 ?.MakeGenericMethod(grpcService);
-            if (method != null) method.Invoke( null, new object[] { builder });
+            if (method != null)
+            {
+                var invokeResult = method.Invoke(null, new object[] {builder}) as IEndpointConventionBuilder;
+                
+                logger.LogInformation($"成功注册GRPC服务：{grpcService.FullName}");
+                
+                if (invokeResult != null)
+                {
+                    logger.LogInformation($"启用GrpcWeb服务：{grpcService.FullName}");
+                    invokeResult.EnableGrpcWeb();
+                }
+                else
+                {
+                    logger.LogInformation($"启用GrpcWeb服务失败：{grpcService.FullName}");
+                }
+            }
+            
+            logger.LogInformation($"结束注册GRPC服务：{grpcService.FullName}");
         }
 
 
-        builder.MapGet("/", async context =>
-        {
-            await context.Response.WriteAsync("服务已启动！");
-        });
+        builder.MapGet("/", async context => { await context.Response.WriteAsync("服务已启动！"); });
     }
 }
