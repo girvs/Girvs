66621dd47baf217e37eaf5d24ede51a7bf2d8de8
kicck <Eternal520@gmail.com>
2026-07-14 02:35:06 -0400

    新增 Girvs.Aspire 模块骨架与首个测试工程

---
+++ Girvs.Aspire/GlobalUsings.cs
@@ -0,0 +1,20 @@
+// Global using directives
+
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Reflection;
+global using Girvs.Configuration;
+global using Girvs.Infrastructure;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using OpenTelemetry;
+global using OpenTelemetry.Metrics;
+global using OpenTelemetry.Trace;
+global using Serilog;
---
+++ tests/Girvs.Aspire.Tests/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Global using directives
+
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Girvs.Configuration;
+global using Girvs.Infrastructure;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Xunit;
