00f2f10aecae7ae0a75174f046b78a5c966cdad3
kicck <Eternal520@gmail.com>
2026-07-22 16:50:45 +0800

    暂时先尝试看看会不会出现dbcontext的问题

--- Girvs.Quartz/GirvsJob.cs
+++ Girvs.Quartz/GirvsJob.cs
@@ -1,15 +1,15 @@
 namespace Girvs.Quartz;
 
-public abstract class GirvsJob(IServiceProvider serviceProvider) : IJob
+public abstract class GirvsJob : IJob
 {
-    public virtual async Task Execute(IJobExecutionContext context)
-    {
-        // 不依赖 Quartz 传入的作用域是否被复用；每次触发都建立独立作用域，
-        // 并在整个异步执行期桥接到 EngineContext，结束后自动还原。
-        using var scope = serviceProvider.CreateScope();
-        using var _ = EngineContext.Current.ChangeCurrentThreadServiceProvider(scope.ServiceProvider);
-        await GirvsExecuteAsync(context);
-    }
-
-    public abstract Task GirvsExecuteAsync(IJobExecutionContext context);
+    public abstract Task Execute(IJobExecutionContext context);
+    // {
+    //     // 不依赖 Quartz 传入的作用域是否被复用；每次触发都建立独立作用域，
+    //     // 并在整个异步执行期桥接到 EngineContext，结束后自动还原。
+    //     using var scope = serviceProvider.CreateScope();
+    //     using var _ = EngineContext.Current.ChangeCurrentThreadServiceProvider(scope.ServiceProvider);
+    //     await GirvsExecuteAsync(context);
+    // }
+    //
+    // public abstract Task GirvsExecuteAsync(IJobExecutionContext context);
 }
